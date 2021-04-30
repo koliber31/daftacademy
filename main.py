9494f17bb6d009f0b76b4d8e76044a8a24c57e79
koliber31 <42122590+koliber31@users.noreply.github.com>
2021-04-30 20:46:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from datetime import date
+
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/hello", response_class = HTMLResponse)
+def root(request: Request):
+    today = date.today().strftime("%Y-%m-%d")
+    return templates.TemplateResponse('index.html', {'request': request, 'today': today})
